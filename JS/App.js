b8a43dc7177520fa1bb6b8402ac09a5a1763917c
John Mac <razooki_rafed@hotmail.com>
2015-12-15 04:35:57 +0100

    Structuur++

---
+++ JS/App.js
@@ -0,0 +1,27 @@
+// new instance of model
+var connection1 = new ConnectionModel({
+  id: 1,
+  datetime: '02-01-2015-01:02:02',
+  port: 'Port Een',
+  value: 0.1
+});
+
+// new instance of model
+var connection2 = new ConnectionModel({
+  id: 2,
+  datetime: '03-01-2015-01:03:03',
+  port: 'Port Twee',
+  value: 0.3
+});
+
+// new instance of model
+var connection3 = new ConnectionModel({
+  id: 3,
+  datetime: '04-01-2015-01:04:04',
+  port: 'Port Drie',
+  value: 0.5
+});
+
+// new instance of collection + add all models
+var Connections = new ConnectionCollection();
+Connections.add([connection1, connection2, connection3]);
