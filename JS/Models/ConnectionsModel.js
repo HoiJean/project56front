9c125b43feea5cc66bd45cb6812c3fcb73ced0fe
John Mac <razooki_rafed@hotmail.com>
2015-12-15 04:35:15 +0100

    Structuur++

--- JS/Models/ConnectionsModel.js
+++ JS/Models/ConnectionsModel.js
@@ -7,27 +7,3 @@ var ConnectionModel = Backbone.Model.extend({
     value: 0.0
   }
 });
-
-// new instance of model
-var connection1 = new ConnectionModel({
-  id: 1,
-  datetime: '02-01-2015-01:02:02',
-  port: 'Port Een',
-  value: 0.1
-});
-
-// new instance of model
-var connection2 = new ConnectionModel({
-  id: 2,
-  datetime: '03-01-2015-01:03:03',
-  port: 'Port Twee',
-  value: 0.3
-});
-
-// new instance of model
-var connection3 = new ConnectionModel({
-  id: 3,
-  datetime: '04-01-2015-01:04:04',
-  port: 'Port Drie',
-  value: 0.5
-});
