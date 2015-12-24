062fbe0592b3c500d48416fc948ba0c5ed8644ee
HoiJean <jean.mail92@gmail.com>
2015-12-24 12:25:28 +0100

    Add Browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var $    = require('gulp-load-plugins')();
+var browserSync = require('browser-sync').create();
 
 var sassPaths = [
   'bower_components/foundation-sites/scss',
@@ -17,10 +18,22 @@ var scriptsPaths = [
     'application/main.js',
 ];
 
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "./"
+    });
+
+    gulp.watch(['scss/**/*.scss'], ['sass']);
+    gulp.watch(['application/**/*.js'], ['scripts']);
+    gulp.watch("*.html").on('change', browserSync.reload);
+});
+
 gulp.task('scripts', function() {
     return gulp.src(scriptsPaths)
     .pipe($.concat('main.js'))
-    .pipe(gulp.dest('js/'));
+    .pipe(gulp.dest('js/'))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('sass', function() {
@@ -32,7 +45,8 @@ gulp.task('sass', function() {
     .pipe($.autoprefixer({
       browsers: ['last 2 versions', 'ie >= 9']
     }))
-    .pipe(gulp.dest('css'));
+    .pipe(gulp.dest('css'))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('default', ['sass', 'scripts'], function() {
