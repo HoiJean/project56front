f02e9871658bc4f4c84f8e930afc80c36b6055ec
Kevin Kroos <kevinkroos@gmail.com>
2015-12-27 20:13:27 +0100

    Connection data ophalen en vullen in tabel

--- application/controllers/RapportController.js
+++ application/controllers/RapportController.js
@@ -2,9 +2,42 @@ app.controller('RapportController', function($scope, connectionService) {
 	$scope.message = "Mijn test";
 
 	connectionService.getConnections().then(function(response) {
-		$scope.connectionsCount = response.data;
+		$scope.connections 		= response.data;
+		$scope.connectionTable	= "";
+		$scope.connectionEvents	= "";
 
-		console.log(response.data);
+		angular.forEach($scope.connections, function(value) {
+			
+			$scope.connectionTable += "<tr>";
+
+			$scope.connectionTable += "<td>" + value.Datetime + "</td>";
+			$scope.connectionTable += "<td>" + value.Value + "</td>";
+			$scope.connectionTable += "<td>" + value.Port + "</td>";
+			$scope.connectionTable += "<td>" + value.Unit_id + "</td>";
+
+			$scope.connectionTable += "</tr>";
+		});
+
+		// console.log(response.data);
+	});
+
+	connectionService.getEvents().then(function(response) {
+		$scope.events 		= response.data;
+		$scope.eventsTable	= "";
+
+		angular.forEach($scope.events, function(value) {
+			
+			$scope.eventsTable += "<tr>";
+
+			// $scope.eventsTable += "<td>" + value.Datetime + "</td>";
+			// $scope.eventsTable += "<td>" + value.Value + "</td>";
+			// $scope.eventsTable += "<td>" + value.Port + "</td>";
+			// $scope.eventsTable += "<td>" + value.Unit_id + "</td>";
+
+			$scope.eventsTable += "</tr>";
+		});
+
+		// console.log(response.data);
 	});
 
 });
