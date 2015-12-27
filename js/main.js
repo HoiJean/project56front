244ba79423b57ae48e49726fb1c8546d94dde04b
Kevin Kroos <kevinkroos@gmail.com>
2015-12-27 21:20:04 +0100

    Bugfixes

--- js/main.js
+++ js/main.js
@@ -32578,24 +32578,24 @@ app.controller('RapportController', function($scope, connectionService) {
 		// console.log(response.data);
 	});
 
-	connectionService.getEvents().then(function(response) {
-		$scope.events 		= response.data;
-		$scope.eventsTable	= "";
+	// connectionService.getEvents().then(function(response) {
+	// 	$scope.events 		= response.data;
+	// 	$scope.eventsTable	= "";
 
-		angular.forEach($scope.events, function(value) {
+	// 	angular.forEach($scope.events, function(value) {
 			
-			$scope.eventsTable += "<tr>";
+	// 		$scope.eventsTable += "<tr>";
 
-			// $scope.eventsTable += "<td>" + value.Datetime + "</td>";
-			// $scope.eventsTable += "<td>" + value.Value + "</td>";
-			// $scope.eventsTable += "<td>" + value.Port + "</td>";
-			// $scope.eventsTable += "<td>" + value.Unit_id + "</td>";
+	// 		// $scope.eventsTable += "<td>" + value.Datetime + "</td>";
+	// 		// $scope.eventsTable += "<td>" + value.Value + "</td>";
+	// 		// $scope.eventsTable += "<td>" + value.Port + "</td>";
+	// 		// $scope.eventsTable += "<td>" + value.Unit_id + "</td>";
 
-			$scope.eventsTable += "</tr>";
-		});
+	// 		$scope.eventsTable += "</tr>";
+	// 	});
 
-		// console.log(response.data);
-	});
+	// 	// console.log(response.data);
+	// });
 
 });
 app.controller('TestController', function($scope) {
